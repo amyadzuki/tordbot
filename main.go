1ea01a0ca2b67f0dca4e20bb6a5d9b835e8045ac
Amy <amyadzuki@gmail.com>
2018-11-13 12:03:33 -0600

    Update main.go

--- main.go
+++ main.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main(args []string) {
-	if len(args) != 2 {
-		fmt.Println("Usage:", args[0], "/path/to/token.dat")
+	if len(args) != 3 {
+		fmt.Println("Usage:", args[0], "/path/to/token.dat /path/to/database.db")
 	}
 	b, err := ioutil.ReadFile(args[1])
+	if err != nil {
+		panic(err)
+	}
+	db, err := sql.Open("sqlite3", args[2])
 	if err != nil {
 		panic(err)
 	}
