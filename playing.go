623c8b81ad96b4d1eedf19e8ffc3b435e3213728
Amy <amyadzuki@gmail.com>
2018-11-14 00:11:52 -0600

    Update playing.go

--- playing.go
+++ playing.go
@@ -18,8 +18,6 @@ func cyclePlayingStatus() {
 		guilds = 0
 		guilds += int64(len(Session.State.Guilds))
 		servers = strconv.FormatInt(guilds, 10) + " Servers"
-		now := time.Now()
-		month := int(now.Month()) - 1
 
 		Session.UpdateStatus(0, servers)
 		time.Sleep(10 * time.Second)
