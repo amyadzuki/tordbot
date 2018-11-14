094242e075eaf27804b21949b7fc3f09e2eeea5f
Amy <amyadzuki@gmail.com>
2018-11-13 23:02:03 -0600

    Update onmsg.go

--- onmsg.go
+++ onmsg.go
@@ -31,40 +31,54 @@ func onMessageCreate(s *discordgo.Session, mc *discordgo.MessageCreate) {
 			return
 		}
 	}
-	nsfw := 1
-	if channel.NSFW {
-		nsfw = 5
-	}
-	very := false
-	if strings.HasPrefix(lowr, "very") {
+	very := 0
+	for strings.HasPrefix(lowr, "very") {
 		payl = payl[4:]
 		lowr = lowr[4:]
-		very = true
+		very++
+		for len(payl) > 0 && payl[0] == ' ' {
+			payl = payl[1:]
+			lowr = lowr[1:]
+		}
 	}
+
 	if strings.HasPrefix(lowr, "sfw") {
 		payl = payl[3:]
 		lowr = lowr[3:]
-		if very {
-			nsfw /= 2 // 0 or 2
-		} // else 1 or 5
+		very = -very
+		for len(payl) > 0 && payl[0] == ' ' {
+			payl = payl[1:]
+			lowr = lowr[1:]
+		}
 	} else if strings.HasPrefix(lowr, "nsfw") {
 		payl = payl[4:]
 		lowr = lowr[4:]
-		if very {
-			nsfw += 2 // 3 or 7
-		} else {
-			nsfw += 4 // 5 or 9
+		for len(payl) > 0 && payl[0] == ' ' {
+			payl = payl[1:]
+			lowr = lowr[1:]
 		}
+	} else {
+		very = 0
 	}
-	if nsfw < 0 || nsfw > 9 {
-		panic("nsfw < 0 || nsfw > 9")
+	nsfw := 5 + 2 * very
+	if nsfw < 0 {
+		nsfw = 0
+	}
+	nsfwadd := uint32(nsfw)
+	if channel.NSFW {
+		nsfw += nsfw
+	}
+	u32nsfw := uint32(nsfw)
+	u32nsfw = prg.Uint32() % u32nsfw
+	u32nsfw = prg.Uint32() % u32nsfw
+	if u32nsfw > 5 && !channel.NSFW {
+		u32nsfw = 5
 	}
 
 	cmdline := strings.Split(payl)
 	if len(cmdline) < 1 {
 		return
 	}
-	embed := new(discordgo.Embed)
 	switch strings.ToLower(cmdline[0]) {
 	case "add":
 		if len(cmdline) < 2 {
@@ -73,12 +87,19 @@ func onMessageCreate(s *discordgo.Session, mc *discordgo.MessageCreate) {
 				"https://docs.google.com/document/d/" +
 				"1NsD_0fASVaixXJAtWIF4tUVRG9vBiSyyiqM_Sb1Hl2c/edit?usp=sharing")
 		}
+		prompt := strings.Join(cmdline[2:], " ")
 		switch strings.ToLower(cmdline[1]) {
 		case "dare":
+			addPrompt("Dares", prompt)
 		case "truth":
+			addPrompt("Truths", prompt)
 		}
 	case "dare":
 	case "help":
+	case "invite":
+		Session.ChannelMessageSend(channelID,
+			"https://discordapp.com/oauth2/authorize?client_id=" +
+			"512117311" + "415648275&scope=bot&permissions=378" + "944")
 	case "truth":
 	}
 }
