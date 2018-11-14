0fec0006feef3d58274dce82378e3b3352b31b2b
Amy <amyadzuki@gmail.com>
2018-11-13 23:10:01 -0600

    Create dbstuff.go

---
+++ dbstuff.go
@@ -0,0 +1,16 @@
+package main
+
+const (
+	AT_ANYWHERE = 1 << iota,
+	AT_HOME_ALONE,
+	AT_HOME,
+	AT_WORK,
+	AT_SCHOOL,
+)
+
+func addPrompt(table string, nsfw uint32, prompt string) {
+	DB.Prepare()
+}
+
+
+
