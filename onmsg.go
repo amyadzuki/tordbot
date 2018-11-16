ce9b7ebcc6ae39e88f5eecd5d1bd95902fcbd9fc
Amy <amyadzuki@gmail.com>
2018-11-16 00:08:14 -0600

    Update onmsg.go

--- onmsg.go
+++ onmsg.go
@@ -12,25 +12,6 @@ func onMessageCreate(s *discordgo.Session, mc *discordgo.MessageCreate) {
 	}
 	message := mc.Message
 	lex(s, message)
-
-	switch strings.ToLower(cmdline[0]) {
-	case "add":
-		if len(cmdline) < 2 {
-			Session.ChannelMessageSend(channel.ID,
-				"Join our public Google Doc here to suggest stuff:\n" +
-				"<https://docs.google.com/document/d/" +
-				"1NsD_0fASVaixXJAtWIF4tUVRG9vBiSyyiqM_Sb1Hl2c/edit?usp=sharing>")
-		}
-		prompt := strings.Join(cmdline[2:], " ")
-		switch strings.ToLower(cmdline[1]) {
-		case "dare":
-			addPrompt(channel.GuildID, channel.ID, author, 1, nsfwadd, at, prompt)
-		case "truth":
-			addPrompt(channel.GuildID, channel.ID, author, 0, nsfwadd, at, prompt)
-		default:
-			Session.ChannelMessageSend(channel.ID,
-				"Unknown add command ``" + cmdline[1] + "\u00b4\u00b4.")
-		}
 }
 
 func onMessageDelete(s *discordgo.Session, md *discordgo.MessageDelete) {
