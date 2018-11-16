ec3dc08ac4d9ef9618282fb3b0e3d64af574066b
Amy <amyadzuki@gmail.com>
2018-11-15 23:23:20 -0600

    Update lex.go

--- lex.go
+++ lex.go
@@ -25,6 +25,10 @@ func lex(s *discordgo.Session, m *discordgo.Message) {
 		return
 	}
 
+	var at, dare uint32
+	very := 1
+	var sfw, nsfw bool
+
 	for {
 		for len(tail) > 0 && tail[0] == ' ' {
 			tail = tail[1:]
@@ -34,16 +38,22 @@ func lex(s *discordgo.Session, m *discordgo.Message) {
 			if (at & AT_HOME) != 0 {
 				at |= AT_HOMEALONE
 			}
-		} else if b, t := chp(tail, "at "); b {
-			tail = t
 		} else if b, t := chp(tail, "anywhere "); b {
 			at |= AT_ANYWHERE
+		} else if b, t := chp(tail, "at "); b {
+			tail = t
 		} else if b, t := chp(tail, "home "); b {
 			at |= AT_HOME
 		} else if b, t := chp(tail, "homealone "); b {
 			at |= AT_HOMEALONE
+		} else if b, t := chp(tail, "nsfw "); b {
+			nsfw = true
 		} else if b, t := chp(tail, "school "); b {
 			at |= AT_SCHOOL
+		} else if b, t := chp(tail, "sfw "); b {
+			sfw = true
+		} else if b, t := chp(tail, "very "); b {
+			very++
 		} else if b, t := chp(tail, "work "); b {
 			at |= AT_WORK
 		}
