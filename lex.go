18aa62217f31cda8eaee7986a8d63aafcc020dc2
Amy <amyadzuki@gmail.com>
2018-11-15 22:37:01 -0600

    Update lex.go

--- lex.go
+++ lex.go
@@ -4,9 +4,16 @@ import (
 	"strings"
 )
 
-func caseHasPrefix(s, prefix string) bool {
+func caseHasLcPrefixSkip(s, prefix string) (bool, string) {
+	if len(s) < len(prefix) {
+		return false, s
+	}
+	len_prefix := len(prefix)
+	if strings.ToLower(s[0:len_prefix]) == prefix {
+		return true, s[len_prefix:]
+	}
+	return false, s
 }
 
 func lex(s *discordgo.Session, m *discordgo.Message) {
-	s
 }
